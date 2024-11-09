dea3ccd00f80672847fd79b75cc35ec06d31183e
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-11-09 18:20:37 +0600

    Created project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import akka.actor.typed.ActorSystem;
-import akka.actor.typed.javadsl.Behaviors;
-import com.typesafe.config.Config;
-
 public class Main {
     public static void main(String[] args) {
 

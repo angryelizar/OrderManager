f726c7842b469d696a78df431cb91cd0692aae38
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-11-14 03:49:04 +0600

    Finish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,6 @@ import console.CLIApplication;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        ActorSystem<OrderManagerMessage> orderManager = ActorSystem.create(OrderManagerActor.createBehavior(), "OrderManager");
-//        orderManager.tell(new CreateOrderMessage("0"));
-//        orderManager.tell(new CreateOrderMessage("1"));
-//        orderManager.tell(new CreateOrderMessage("2"));
-//        orderManager.tell(new CreateOrderMessage("100"));
-//        orderManager.tell(new GetListOfOrdersMessage());
         CLIApplication app = new CLIApplication();
     }
 }
